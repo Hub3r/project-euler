78aeeb63e2ff9bb162bd71731dd84f333e642991
Jacob Huber <jphuber2@ncsu.edu>
2019-11-28 22:17:10 -0500

    finished programming solutions in python

--- projectEuler/problem3/largestPrimeFactor.py
+++ projectEuler/problem3/largestPrimeFactor.py
@@ -1,5 +1,23 @@
-number = 600851475143
-factors = []
-for x in range(1, number):
-    if number % x != 0:
-        factors.append(x)
+def main():
+    number = 600851475143
+    primeList = []
+    x = 0
+    while number != 1:
+        x += 1
+        if primeChecker(x) == x:
+            if number % x == 0:
+                primeList.append(x)
+                number = number / x
+                x = 0
+    print(primeList)
+
+def primeChecker(value):
+    if value > 1:
+        for i in range(2, value/2):
+            if value % i == 0:
+                return 0
+        return value
+    else:
+        return 0
+
+main()
